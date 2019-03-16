f84c08c50df1e2ef4836c7ec1effb5580df7763a
Bo Yao <byao@mymakecents.com>
2019-03-15 22:19:25 -0400

    filters working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,26 @@
 class ProjectsController < ApplicationController
+  def filter_range(projects, param_name)
+    filter_name = "filter_#{param_name}"
+    if params[param_name].present?
+      return projects.send(filter_name, params[:actual_start_date], nil, nil)
+    end
+    param_min = params["#{param_name}_min"]
+    param_max = params["#{param_name}_max"]
+    if param_min.present? or param_max.present?
+      projects.send(filter_name, nil, param_min, param_max)
+    else
+      projects
+    end
+  end
   def show
     @projects = Project.where(nil)
     @projects = @projects.search(params[:search]) if params[:search].present?
+    @projects = filter_range(@projects, "actual_start_date")
+    @projects = filter_range(@projects, "actual_end_date")
+    @projects = filter_range(@projects, "planned_end_date")
+    @projects = filter_range(@projects, "final_estimate")
+    @projects = filter_range(@projects, "total_spending")
+
     render json: @projects
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,28 @@
 class Project < ApplicationRecord
+  def self.filter_range(col_name)
+    -> (amount, amount_min, amount_max) do
+      if amount
+        where col_name => amount
+      else
+        filter = where nil
+        if amount_min
+          filter = filter.where "\"#{col_name}\" >= ?", amount_min
+        end
+        if amount_max
+          filter = filter.where "\"#{col_name}\" <= ?", amount_max
+        end
+        filter
+      end
+    end
+  end
+
   scope :search, -> (search) { where '"Project School Name" like ? OR "Project Description" like ?',
                                      "%#{search}%", "%#{search}%" }
+  scope :filter_actual_start_date, filter_range("Project Phase Actual Start Date")
+  scope :filter_actual_end_date, filter_range("Project Phase Actual End Date")
+  scope :filter_planned_end_date, filter_range("Project Phase Planned End Date")
+  scope :filter_budget, filter_range("Project Budget Amount")
+  scope :filter_final_estimate , filter_range("Final Estimate of Actual Costs Through End of Phase Amount")
+  scope :filter_total_spending, filter_range("Total Phase Actual Spending Amount")
+
 end
